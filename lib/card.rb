6655790e4cb19dd47b3e225272d8c854543c721b
Jasmine <jasylop@gmail.com>
2020-09-27 22:20:45 -0700

    raised argument errors for invalid values and created helper method, convert_facecard

--- lib/card.rb
+++ lib/card.rb
@@ -5,6 +5,9 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    raise ArgumentError, "#{suit} is not a valid Suit" unless [:hearts, :spades, :clubs, :diamonds].include?(suit)
+    raise ArgumentError, "#{value} is not a valid value" if value < 1 || value > 13
+
     @value = value
     @suit = suit
     
@@ -12,7 +15,21 @@ class Card
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{convert_facecard || value} of #{suit.to_s}"
+  end
+
+  # helper method for to_s
+  def convert_facecard
+    case value
+    when 13
+      return "King"
+    when 12
+      return "Queen"
+    when 11
+      return "Jack"
+    when 1
+      return "Ace"
+    end
   end
 
 end
